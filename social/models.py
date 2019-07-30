f2761dbde393b8490ce0598ae2394c69bca0a0f6
FE1979 <ronin911@gmail.com>
2019-07-30 10:33:04 +0300

    Added Post and Like models

--- social/models.py
+++ social/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """ Post created by API user """
+
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, blank=True, default='')
+    body = models.TextField()
+    owner = models.ForeignKey('auth.user', related_name='posts',
+                              on_delete=models.CASCADE)
+
+class Like(models.Model):
+    """ Like for Post
+        Only owner can modify like
+        Like is related to the Post
+    """
+
+    owner = models.ForeignKey('auth.user', related_name='likes',
+                              on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes',
+                             on_delete=models.CASCADE)
