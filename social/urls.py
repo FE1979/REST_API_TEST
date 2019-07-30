74715d7bf8acadad419be11bcac675621893b9dd
FE1979 <ronin911@gmail.com>
2019-07-30 11:27:05 +0300

    Social urls.py configured

---
+++ social/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from social import views
+
+
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('posts/', views.PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+    path('likes/', views.LikeList.as_view(), name='like-list')
+])
