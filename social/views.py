e6ec05b7d25c9aaf5bd252e73b1f73e73b9009af
FE1979 <ronin911@gmail.com>
2019-07-30 12:26:21 +0300

    Added api_view to the social views

--- social/views.py
+++ social/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
 from django.contrib.auth.models import User
 
@@ -48,3 +49,12 @@ class LikeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'posts': reverse('post-list', request=request, format=format),
+        'likes': reverse('like-list', request=request, format=format)
+    })
