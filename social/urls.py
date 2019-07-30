ca47d9b21876ee6474721fbc0d6b59f7139ca627
FE1979 <ronin911@gmail.com>
2019-07-30 12:24:26 +0300

    Link api_view to the url

--- social/urls.py
+++ social/urls.py
@@ -5,6 +5,7 @@ from social import views
 
 # API endpoints
 urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
     path('posts/', views.PostList.as_view(), name='post-list'),
