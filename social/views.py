2f5365646896890b3407ece0614626eddc30803e
FE1979 <ronin911@gmail.com>
2019-07-30 11:17:55 +0300

    Views created

--- social/views.py
+++ social/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import generics, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from social.models import Post, Like
+from social.serializers import UserSerializer, PostSerializer, LikeSerializer
+
+
+class UserList(generics.ListAPIView):
+    """ Render User list """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """ Render User detail """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    """ Render Posts list """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """ Render Post detail """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class LikeList(generics.ListCreateAPIView):
+    """ Render Like list """
+
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
