6156b0c3bfd8d8ee2e00c5db9e2002f9df70bd59
FE1979 <ronin911@gmail.com>
2019-08-01 17:21:01 +0300

    Bot.py renamed to bot_users.py and cleaned from unneccessary code

---
+++ bot/bot_users.py
@@ -0,0 +1,84 @@
+import requests
+import json
+
+
+class User():
+    """ User class """
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.auth = {'username': self.username, 'password': self.password}
+
+
+    def sign_up(self, url):
+        """ Sing up new user
+            input:  sign up url
+            return: requests.response
+        """
+
+        resp = requests.post(url, data=self.auth)
+        return resp
+
+
+    def get_tokens(self, url):
+        """ Gets token for user
+            input: tokens url
+        """
+
+        resp = requests.post(url, data=self.auth)
+        tokens = json.loads(resp.text)
+        self.access_token = tokens['access']
+        self.refresh_token = tokens['refresh']
+
+        return resp
+
+
+    def get_header(self):
+        header = {"Content-Type": "application/json", "Authorization": "Bearer "
+                   + self.access_token()}
+
+        return header
+
+
+    def create_post(self, url, post):
+        """ Creates post by sending post request
+            input url:  posts url
+            input post: post with 'title' and 'body' desribed
+            input_type: dict
+            return:     requests.response
+        """
+
+        resp = requests.post(url, headers=self.get_header(),
+                             data=json.dumps(post))
+
+        return resp
+
+
+    def like_post(self, url, post_number):
+        """ Likes post
+            input url:  posts url
+            input:      post_number
+            input_type: integer
+            return:     requests.response
+        """
+
+        resp = requests.post(url + f'{post_number}/like',
+            headers=self.get_header())
+
+        return resp
+
+
+    def unlike_post(self, url,  post_number):
+        """ Unlikes post
+            input url:  posts url
+            input:      post_number
+            input_type: integer
+            return:     requests.response
+        """
+
+        resp = requests.post(url + f'{post_number}/unlike',
+            headers=self.get_header())
+
+        return resp
