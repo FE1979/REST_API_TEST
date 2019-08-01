f1e1dbe0366243ed48619315d1d9c4f956fa762e
FE1979 <ronin911@gmail.com>
2019-08-01 09:07:16 +0300

    Creation Post method rewritten. Explicitly defined access class JWToken to Post list view

--- social/views.py
+++ social/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics, renderers
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
+from rest_framework import generics, renderers, status, viewsets
 from rest_framework.views import APIView
-from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from django.contrib.auth.models import User
 
@@ -37,9 +38,14 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostDetail(LikedMixin, viewsets.ModelViewSet):
